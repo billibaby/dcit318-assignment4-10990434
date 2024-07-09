c8442f16ac09cce76882a4430a9d0f7908298c92
billibaby <aamensah056@st.ug.edu.gh>
2024-07-09 01:59:23 +0000

    question1 commit
    question1 commit

---
+++ Question1/Form1.cs
@@ -0,0 +1,33 @@
+namespace Question1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = Username.Text;
+            string password = Password.Text;
+
+            _ = MessageBox.Show($"Username: {username}\nPassword: {password}", "Login Information");
+        }
+    }
+}
